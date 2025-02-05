bb58bd6b22dfd9b9f444c743e1fd236bca4ccaeb
Akshay <gupta.akshay2003@gmail.com>
2025-02-05 09:56:25 +0530

    firse commit kr diya

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 const data = fs.readFileSync("./data.txt","utf-8");
 console.log(data);
+if(data.match("H")){
+    console.log("H is present")
+    const changeddata = data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",changeddata);
+}
+
+// fs.unlinkSync("./data2.txt","ABSE promise");
+// fs.writeFileSync("./data1.txt","ABES promise");
+// fs.appendFileSync("./data1.txt","AKTU promise");
+// fs.renameSync("./data1.txt","./data2.txt");
